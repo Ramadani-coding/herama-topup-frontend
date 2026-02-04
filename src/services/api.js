ae45a178ce61260c03dedbfa7ae918c3b11a8928
Ramadani-coding <rama.id2210@gmail.com>
2026-02-04 13:27:04 +0800

    update API

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // Gunakan domain publik yang sudah Anda tunnel lewat Cloudflare
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://api.herama.my.id/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
